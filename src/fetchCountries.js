82b285d346b0f440567b334dc3d4b5846f981124
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-08-14 21:52:48 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,9 @@
 const BASE_URL = 'https://restcountries.com/v3.1';
 
-const fetchCountries = name => {
+export const fetchCountries = name => {
   return fetch(
     `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
   ).then(response => {
     return response.json();
   });
 };
-export default { fetchCountries };
