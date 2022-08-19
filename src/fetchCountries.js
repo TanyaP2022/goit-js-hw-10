cfe376d6a02d3a2ca777f7351d191e147743394a
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-08-19 22:18:33 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,9 +1,16 @@
+import { Notify } from 'notiflix';
+
 const BASE_URL = 'https://restcountries.com/v3.1';
 
 export const fetchCountries = name => {
   return fetch(
     `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
   ).then(response => {
+    if (!response) {
+      throw new Error(
+        Notify.failure('Oops, there is no country with that name')
+      );
+    }
     return response.json();
   });
 };
