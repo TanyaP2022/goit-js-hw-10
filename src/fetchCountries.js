6d252136428ab7bbe4386ccd57694a4727604f8c
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-08-19 22:23:43 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,8 +1,8 @@
 import { Notify } from 'notiflix';
 
-const BASE_URL = 'https://restcountries.com/v3.1';
-
 export const fetchCountries = name => {
+  const BASE_URL = 'https://restcountries.com/v3.1';
+
   return fetch(
     `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
   ).then(response => {
